d57f8ab533a588fcd8e281d4e4fd19d5dc4a3e3f
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-05 18:24:57 -0300

    a

--- flex-e-grid-main/script-inicial.js
+++ flex-e-grid-main/script-inicial.js
@@ -4,3 +4,8 @@ const menu = document.querySelector('.menu-lateral')
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+//Mudar a cor do <p class="cartão__info cartao__info--visualizacao">33 Visualizações</p> para azul
+/* var pegaCor = document.querySelector(".cartao__info--visualizacao");
+var mudaCor = pegaCor.textContent
+console.log(mudaCor);
+mudaCor.body.style.fontColor = "white"*/
