764caf7aa5af2f0882e2098b658b29aba9a514ae
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-16 13:20:59 -0300

    a

---
+++ Tiger school/script.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("#menubotton");
+const menuNav = document.querySelector(".navbar");
+
+botao.addEventListener("click", () => {
+    console.log("to pegando");
+
+})
