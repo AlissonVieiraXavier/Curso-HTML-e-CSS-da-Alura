f20ff9fe23f44a96a452d2557333bf0acf3518a5
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-16 15:12:33 -0300

    a

--- Tiger school/script.js
+++ Tiger school/script.js
@@ -5,3 +5,20 @@ botao.addEventListener("click", function () {
     menuNav.classList.toggle('navbar--ativo')
 
 })
+
+
+//carrosel
+
+const imgs = document.getElementById("img-carrosel");
+const img = document.querySelectorAll("#img-carrosel img")
+
+var idx = 0;
+function carrosel() {
+    idx++;
+
+    if (idx > imgs.length - 1) {
+        idx = 0;
+    }
+    img.style.transform = 'translateX()'
+
+}
