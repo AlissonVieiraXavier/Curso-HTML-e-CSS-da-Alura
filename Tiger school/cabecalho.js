8e5f5355e7ce7c3204d433ce753e84db2880a490
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-17 17:32:38 -0300

    a

--- Tiger school/cabecalho.js
+++ Tiger school/cabecalho.js
@@ -7,19 +7,5 @@ botao.addEventListener("click", function () {
 })
 
 
-//carrosel
 
-var imgs = document.getElementById("img-carrosel");
-var img = document.querySelectorAll("#img-carrosel img")
 
-var idx = 0;
-function carrosel() {
-    idx++;
-
-    if (idx > img.length - 1) {
-        idx = 0;
-    }
-    imgs.style.transform = `translateX(${-idx * 500}px)`;
-
-}
-setInterval(carrosel(), 1000);
